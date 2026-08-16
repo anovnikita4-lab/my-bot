19f439d5c1e629589904c028bb0e595d0df0aede
anovnikita4-lab <anovnikita4@gmail.com>
2026-08-16 15:46:38 +1000

    Update database.py

--- database.py
+++ database.py
@@ -565,3 +565,25 @@ def get_partners():
     conn.close()
 
     return users
+    
+def get_partners():
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT 
+            telegram_id,
+            username,
+            full_name,
+            balance
+        FROM users
+        """
+    )
+
+    users = cur.fetchall()
+
+    conn.close()
+
+    return users
